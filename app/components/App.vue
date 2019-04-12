8af3208422e77877509627f69fa029086dd4e617
leo0828 <332488573@qq.com>
2019-04-12 14:28:34 +0800

    update App

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,74 +1,141 @@
 <template>
-  <Page>
-    <ActionBar
-      title="My Tasks"
-      android:flat="true"
-    />
-    <TabView
-      android:tabBackgroundColor="#53ba82"
-      android:tabTextColor="#c4ffdf"
-      android:selectedTabTextColor="#ffffff"
-      androidSelectedTabHighlightColor="#ffffff"
-    >
-      <TabViewItem title="TO DO">
-        <!-- Positions an input field, a button, and the list of tasks in a vertical stack. -->
+  <Page class="page">
+    <ActionBar title="备忘录" class="action-bar" />
+
+    <TabView height="100%" selectedTabTextColor="#53ba82" tabTextFontSize="15">
+      <TabViewItem title="待办事项">
         <StackLayout orientation="vertical" width="100%" height="100%">
 
           <GridLayout columns="2*,*" rows="*" width="100%" height="25%">
-            <!-- Configures the text field and ensures that pressing Return on the keyboard produces the same result as tapping the button. -->
-            <TextField col="0" row="0" v-model="textFieldValue" hint="Type new task..." editable="true" @returnPress="onButtonTap" /> 
-
-            <Button col="1" row="0" text="Add task" @tap="onButtonTap" />
+            <TextField col="0" row="0" v-model="textFieldValue" hint="新建待办事项..." editable="true"
+              @returnPress="onButtonTap" />
+            <Button col="1" row="0" text="创建" @tap="onButtonTap" />
           </GridLayout>
 
-          <ListView class="list-group" for="todo in todos" @itemTap="onItemTap" style="height:75%">
+          <ListView separatorColor="transparent" class="list-group" for="todo in todos" @itemTap="onItemTap"
+            style="height:75%">
             <v-template>
-              <Label :text="todo.name" class="list-group-item-heading" />
+              <Label id="active-task" :text="todo.name" class="list-group-item-heading" />
             </v-template>
           </ListView>
         </StackLayout>
       </TabViewItem>
-      <TabViewItem title="COMPLETED">
-        <GridLayout
-          columns="*"
-          rows="*"
-        >
-          <Label
-            class="message"
-            :text="'Hello '+name"
-            col="0"
-            row="0"
-          />
-        </GridLayout>
+      <TabViewItem title="已办事项">
+        <ListView separatorColor="transparent" class="list-group" for="done in dones" @itemTap="onDoneTap"
+          style="height:75%">
+          <v-template>
+            <Label id="completed-task" :text="done.name" class="list-group-item-heading" />
+          </v-template>
+        </ListView>
       </TabViewItem>
     </TabView>
   </Page>
 </template>
 
-<script >
-import { mapState } from "vuex";
-export default {
-  computed: {
-    ...mapState(["name"])
-  },
-  data() {
-    return {
-      msg: "Hello World!"
-    };
-  }
-};
+<script>
+  export default {
+    methods: {
+      onItemTap: function (args) {
+        action("操作", "取消", ["标记为已办事项", "删除"]).then(result => {
+          console.log(result);
+          switch (result) {
+            case "标记为已办事项":
+              this.dones.unshift(args.item);
+              this.todos.splice(args.index, 1);
+              break;
+            case "删除":
+              this.todos.splice(args.index, 1);
+              break;
+            default:
+              break;
+          }
+        });
+      },
+
+      onDoneTap: function (args) {
+        action("操作", "取消", ["标记为待办事项", "删除"]).then(result => {
+          console.log(result);
+          switch (result) {
+            case "标记为待办事项":
+              this.todos.unshift(args.item);
+              this.dones.splice(args.index, 1);
+              break;
+            case "删除":
+              this.dones.splice(args.index, 1);
+              break;
+            default:
+              break;
+          }
+        });
+      },
+
+      onButtonTap() {
+        console.log("New task added: " + this.textFieldValue + ".");
+        this.todos.unshift({
+          name: this.textFieldValue
+        });
+        this.textFieldValue = "";
+      }
+    },
+
+    data() {
+      return {
+        dones: [],
+        todos: [],
+        textFieldValue: ""
+      };
+    }
+  };
 </script>
 
 <style scoped>
-ActionBar {
-  background-color: #53ba82;
-  color: #ffffff;
-}
+  TextField {
+    font-size: 20;
+    color: #53ba82;
+    margin-top: 10;
+    margin-bottom: 10;
+    margin-right: 5;
+    margin-left: 20;
+  }
 
-.message {
-  vertical-align: center;
-  text-align: center;
-  font-size: 20;
-  color: #333333;
-}
+  button {
+    font-size: 20;
+    font-weight: bold;
+    color: white;
+    background-color: #53ba82;
+    height: 40;
+    margin-top: 10;
+    margin-bottom: 10;
+    margin-right: 10;
+    margin-left: 10;
+    border-radius: 20px;
+  }
+
+  .home-panel {
+    vertical-align: center;
+    font-size: 20;
+    margin: 15;
+  }
+
+  .description-label {
+    margin-bottom: 15;
+  }
+
+  #active-task {
+    font-size: 20;
+    font-weight: bold;
+    color: #53ba82;
+    margin-left: 20;
+    padding-top: 5;
+    padding-bottom: 10;
+  }
+
+  #completed-task {
+    font-size: 20;
+    color: #d3d3d3;
+    margin-left: 20;
+    padding-top: 5;
+    padding-bottom: 10;
+    text-decoration: line-through;
+  }
 </style>
